4c2b61594adf7ae8f95d11a289a01858f1f85567
Pedro <pfcaraujo@uolinc.com>
2020-03-08 17:31:06 -0300

    Removes Storyboard

---
+++ MarvelFlutter/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MarvelFlutter
+//
+//  Created by Pedro Felipe Coloma de Araujo on 08/03/20.
+//  Copyright © 2020 Pedro Felipe Coloma de Araujo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
