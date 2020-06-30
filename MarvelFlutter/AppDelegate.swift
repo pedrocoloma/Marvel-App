80c65770856c41b4ab98e03f427ed528b7b0915c
Pedro <pfcaraujo@uolinc.com>
2020-06-30 11:55:05 -0300

    Implements Method Channel

--- MarvelFlutter/AppDelegate.swift
+++ MarvelFlutter/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        flutterEngine.run()
+        setupFlutter()
         return true
     }
 
@@ -33,5 +33,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+extension AppDelegate {
+    func setupFlutter() {
+        flutterEngine.run()
+        let defaults = UserDefaults.standard
+        
+        let flutterChannel = FlutterMethodChannel(name: "marvel.flutter", binaryMessenger: flutterEngine.binaryMessenger)
+        flutterChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            switch call.method {
+            case "getDefaults":
+                let isDarkModeOn = defaults.string(forKey: "isDarkModeOn") ?? ""
+                let language = defaults.string(forKey: "language") ?? ""
+                let defaultsMap: [String: String] = ["isDarkModeOn": isDarkModeOn, "language": language]
+                
+                result(defaultsMap)
+            case "setDefaults":
+                if let args = call.arguments as? [String: String] ,
+                    let language = args["language"],
+                    let isDarkModeOn = args["isDarkModeOn"] {
+                    defaults.set(language, forKey: "language")
+                    defaults.set(isDarkModeOn, forKey: "isDarkModeOn")
+                }
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
     }
 }
